62f822efdd7e59c7045f78e2ee556c9b85c1abb2
6210612831 <60337808+6210612831@users.noreply.github.com>
2021-03-08 12:56:38 +0700

    def get_size

---
+++ Main.py
@@ -0,0 +1,16 @@
+import cv2 as cv
+import matplotlib.pyplot as plt
+import numpy as np
+import os
+import IPython
+import scipy
+import sklearn
+
+img = cv.imread('images/02_g.jpg')
+plt.imshow(img)
+
+
+def get_size(pic):
+    width=pic.shape[0]
+    height=pic.shape[1]
+    return [pic,width,height]
