9d614305d522ed9fe091c48f2e3e653a1b0f5392
6210612567 <60337601+6210612567@users.noreply.github.com>
2021-03-20 14:35:04 +0700

    Update freq_hist.py

--- freq_hist.py
+++ freq_hist.py
@@ -7,16 +7,8 @@ import scipy
 import sklearn
 from PIL import Image
 
-img = cv.imread("1ffa933b-8d87-11e8-9daf-6045cb817f5b..jpg")
+img = cv.imread("02_g.jpg")
 img = cv.cvtColor(img, cv.COLOR_BGR2RGB)
-
-# remove
-def remove_zero_hist(hist) :
-    while True :
-        (minVal, maxVal, minLoc, maxLoc) = cv.minMaxLoc(hist)
-        hist[np.where(hist >= maxVal-1)] = 0
-        if np.argmax(hist) > 10 :
-            return hist
 
 # resize image
 
@@ -24,45 +16,18 @@ dim = (1080,720)
 
 img = cv.resize(img, dim, interpolation = cv.INTER_AREA)
 
-# b,g,r freq
+def find_mask(img) :
+    gray = cv.cvtColor(img,cv.COLOR_BGR2GRAY)
+    ret,mask =cv.threshold(gray,10,255,cv.THRESH_BINARY)
+    return mask
 
-def freq_histr(img):
-    color = ('b','g','r')
-    for i,col in enumerate(color):
-        histr = cv.calcHist([img],[0],None,[256],[0,256])
-        histr = remove_zero_hist(histr)
-        plt.plot(histr,'b')
-        plt.xlim([0,256])
-        hist = np.histogram(histr)
-        avg_b = np.average(hist[1])
-        avg_b = str(avg_b)
+def meanStd_nomask(img,img_mask) :
+    img_mean,img_std = cv.meanStdDev(img,mask = img_mask)
+    return (img_mean[0][0],img_std[0][0])
 
-    for i,col in enumerate(color):
-        hist = 0 
-        histr = cv.calcHist([img],[1],None,[256],[0,256])
-        histr = remove_zero_hist(histr)
-        plt.plot(histr,'g')
-        plt.xlim([0,256])
-        hist = np.histogram(histr)
-        avg_g = np.average(hist[1])
-        avg_g = str(avg_g)
-
-    for i,col in enumerate(color):
-        hist = 0
-        histr = cv.calcHist([img],[2],None,[256],[0,256])
-        histr = remove_zero_hist(histr)
-        plt.plot(histr,'r')
-        plt.xlim([0,256])
-        hist = np.histogram(histr)
-        avg_r = np.average(hist[1])
-        avg_r = str(avg_r)
-    freq_ans = [avg_b,avg_g,avg_r]
-    return freq_ans
-
-#freq_histr = 'blue channel  : ' + avg_b + '\ngreen channel : ' + avg_g+ '\nred channel   : ' + avg_r
-
-freq_show = freq_histr(img)
+freq_show = meanStd_nomask(img,find_mask(img))
 print(freq_show)
+
 
 
 
